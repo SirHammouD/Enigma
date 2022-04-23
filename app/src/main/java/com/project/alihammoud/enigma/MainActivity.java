5e662752262168a46315d7f48b0084769c913d33
Ali <AliHammoud@my.unt.edu>
2022-04-23 14:11:43 -0500

    Testing

--- app/src/main/java/com/project/alihammoud/enigma/MainActivity.java
+++ app/src/main/java/com/project/alihammoud/enigma/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Testing
 
 
     }
