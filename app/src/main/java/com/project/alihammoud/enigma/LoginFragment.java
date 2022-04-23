0d0b79d9edffe7af2e987030f4211b0898dfff07
Hadi Dayeh <hadi.dayeh@gmail.com>
2022-04-23 23:26:21 +0300

    login created

--- app/src/main/java/com/project/alihammoud/enigma/LoginFragment.java
+++ app/src/main/java/com/project/alihammoud/enigma/LoginFragment.java
@@ -10,18 +10,9 @@ import android.view.ViewGroup;
 
 
 public class LoginFragment extends Fragment {
-
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-      View view = inflater.inflate(R.layout.fragment_login, container, false);
-
-
-
-
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
 
         return view;
     }
