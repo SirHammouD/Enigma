18ee0c55d4ed161410fd66cbb6cb57d3f7f997e9
Ali <AliHammoud@my.unt.edu>
2022-04-23 14:18:04 -0500

    login

---
+++ app/src/main/java/com/project/alihammoud/enigma/LoginFragment.java
@@ -0,0 +1,28 @@
+package com.project.alihammoud.enigma;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoginFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+
+
+
+
+
+        return view;
+    }
+}
