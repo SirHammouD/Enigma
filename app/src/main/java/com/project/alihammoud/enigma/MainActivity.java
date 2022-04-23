0e0b4edc1c8daed28920dbbed188265809bdf14e
Ali <AliHammoud@my.unt.edu>
2022-04-23 14:08:01 -0500

    Porject Created

---
+++ app/src/main/java/com/project/alihammoud/enigma/MainActivity.java
@@ -0,0 +1,29 @@
+package com.project.alihammoud.enigma;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.project.alihammoud.enigma.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+}
