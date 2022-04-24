967a6ca9a1d86addee56cbfbd3253eb89fbda331
Ali <AliHammoud@my.unt.edu>
2022-04-23 23:47:16 -0500

    Questions and Answers layouts added

--- app/src/main/java/com/project/alihammoud/enigma/AnswerFragment.java
+++ app/src/main/java/com/project/alihammoud/enigma/AnswerFragment.java
@@ -1,21 +1,19 @@
 package com.project.alihammoud.enigma;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 
-public class ExploreFragment extends Fragment {
-
-
+public class AnswerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_answer, container, false);
 
 
